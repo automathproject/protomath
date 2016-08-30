90870aa9218dabe156ef893f60d6216196a47136
maxime <maxime@maxime-pc>
2016-08-30 09:29:35 +0200

    probleme ligne 34

--- automath3/src/automath3/settings.py
+++ automath3/src/automath3/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'models.apps.PollsConfig',
+    'exobase.apps.ExobaseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
